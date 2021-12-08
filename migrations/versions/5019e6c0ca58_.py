625c1286f5ffdfe87a801edc41dc390f92aa5b2f
Francesco Kotopulos De Angelis <Koto@MacBook-Pro-di-Francesco.local>
2021-12-08 13:15:32 +0100

    Change run.sh

--- migrations/versions/5019e6c0ca58_.py
+++ migrations/versions/5019e6c0ca58_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 30d938dd65dd
+Revision ID: 5019e6c0ca58
 Revises: 
-Create Date: 2021-12-07 11:43:43.614065
+Create Date: 2021-12-08 11:47:22.862202
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '30d938dd65dd'
+revision = '5019e6c0ca58'
 down_revision = None
 branch_labels = None
 depends_on = None
